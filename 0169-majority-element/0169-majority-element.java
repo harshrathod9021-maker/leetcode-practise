db11c341d6989a10836715a14d4f81a02de6ef50
harshrathod9021-maker <harshrathod9021@gmail.com>
2026-08-24 23:48:22 -0600

    Time: 13 ms (32.23%), Space: 52.3 MB (95.1%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,8 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        return nums[n/2];
-        
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        // Count frequency
+        for (int i = 0; i < nums.length; i++) {
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int k = nums.length / 2;
+
+        // Check which number occurs more than n/2 times
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+
+            if (entry.getValue() > k) {
+                return entry.getKey();
+            }
+        }
+
+        return -1;
     }
 }
