6419fd468a6ec266e2e183c57f760cd9f53eda33
harshrathod9021-maker <harshrathod9021@gmail.com>
2026-08-24 22:20:04 -0600

    Time: 11 ms (66.94%), Space: 46.7 MB (71.86%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,35 +1,102 @@
+// class Solution {
+//     public List<Integer> findAnagrams(String s, String p) {
+//         int plen = p.length();
+//         List<Integer> k = new ArrayList<>();
+//         boolean n;
+//         for(int i = 0;i<=s.length()-plen;i++){
+//             n = isanagram(s.substring(i,i+plen),p);
+//             if(n){
+//                 k.add(i);
+//             }
+//         }
+//         return k;
+        
+//     }
+
+//     private boolean isanagram(String k,String p){
+//          if (k.length() != p.length()) {
+//             return false;
+//         }
+//         int[] count = new int[26];
+
+//         for (int i = 0; i < k.length(); i++) {
+//             count[k.charAt(i) - 'a']++;
+//             count[p.charAt(i) - 'a']--;
+//         }
+
+//         for (int i = 0; i < 26; i++) {
+//             if (count[i] != 0) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int plen = p.length();
-        List<Integer> k = new ArrayList<>();
-        boolean n;
-        for(int i = 0;i<=s.length()-plen;i++){
-            n = isanagram(s.substring(i,i+plen),p);
-            if(n){
-                k.add(i);
+        List<Integer> ans = new ArrayList<>();
+
+        if (p.length() > s.length()) return ans;
+
+        int[] pFreq = new int[26];
+        int[] window = new int[26];
+
+        for (char c : p.toCharArray()) {
+            pFreq[c - 'a']++;
+        }
+
+        int k = p.length();
+
+        for (int i = 0; i < k; i++) {
+            window[s.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(pFreq, window)) {
+            ans.add(0);
+        }
+
+        for (int i = k; i < s.length(); i++) {
+            window[s.charAt(i) - 'a']++;
+            window[s.charAt(i - k) - 'a']--;
+
+            if (Arrays.equals(pFreq, window)) {
+                ans.add(i - k + 1);
             }
         }
-        return k;
-        
-    }
 
-    private boolean isanagram(String k,String p){
-         if (k.length() != p.length()) {
-            return false;
-        }
-        int[] count = new int[26];
-
-        for (int i = 0; i < k.length(); i++) {
-            count[k.charAt(i) - 'a']++;
-            count[p.charAt(i) - 'a']--;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                return false;
-            }
-        }
-
-        return true;
+        return ans;
     }
 }
