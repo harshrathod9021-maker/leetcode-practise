c2c0158676248d2a435b6d6efcdd879c4d3bca5b
harshrathod9021-maker <harshrathod9021@gmail.com>
2026-08-29 10:07:06 -0600

    Time: 2 ms (41.88%), Space: 43.2 MB (8.63%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c == '(') {
+                stack.push(c);
+            }
+            else {
+                if(stack.isEmpty() || stack.peek() == ')')
+                    stack.push(c);
+                else
+                    stack.pop();
+            }
+        }
+
+        return stack.size();
+    }
+}
