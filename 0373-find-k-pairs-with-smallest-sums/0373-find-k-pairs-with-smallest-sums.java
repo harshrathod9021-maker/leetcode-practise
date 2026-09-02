03714c0799f223cfa4f3e1090a91d8f8d5f42380
harshrathod9021-maker <harshrathod9021@gmail.com>
2026-09-01 21:51:10 -0600

    Time: 27 ms (99.45%), Space: 134.2 MB (83.22%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,22 +1,67 @@
 class Solution {
-    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        
-		//In the minHeap array, 0th element refers to the curr element in nums1 and 1st element refers to curr element in nums2 
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> (a[0] + a[1]) - (b[0] + b[1]));
-        
-		// The 2nd element in the minHeap is the index of nums2, the corresponding element of nums2 is in index1 of minHeap
-        for(int i=0; i < nums1.length && i < k; i++)
-            minHeap.add(new int[]{nums1[i], nums2[0], 0});
-        
-        List<List<Integer>> result = new ArrayList<>();
-        
-        for(int i=0; i < k && !minHeap.isEmpty(); i++){
-            int[] curr = minHeap.poll();
-            result.add(List.of(curr[0], curr[1]));
-            int nums2Idx = curr[2];
-            if(nums2Idx < nums2.length - 1)
-                minHeap.add(new int[]{curr[0], nums2[nums2Idx + 1], nums2Idx + 1});
+
+    public List<List<Integer>> kSmallestPairs(
+            int[] nums1,
+            int[] nums2,
+            int k) {
+
+        // Min Heap
+        // int[0] = value from nums1
+        // int[1] = value from nums2
+        // int[2] = index in nums2
+
+        PriorityQueue<int[]> minHeap =
+            new PriorityQueue<>(
+                (a, b) ->
+                    (a[0] + a[1]) - (b[0] + b[1])
+            );
+
+        // Put the first pair of each nums1 row
+        for (int i = 0;
+             i < nums1.length && i < k;
+             i++) {
+
+            minHeap.add(
+                new int[]{
+                    nums1[i],
+                    nums2[0],
+                    0
+                }
+            );
         }
+
+        List<List<Integer>> result =
+            new ArrayList<>();
+
+        // Get k smallest pairs
+        for (int i = 0;
+             i < k && !minHeap.isEmpty();
+             i++) {
+
+            // Get smallest pair
+            int[] curr = minHeap.poll();
+
+            // Add pair to result
+            result.add(
+                List.of(curr[0], curr[1])
+            );
+
+            // Current index in nums2
+            int nums2Idx = curr[2];
+
+            // Move to next element in nums2
+            if (nums2Idx < nums2.length - 1) {
+
+                minHeap.add(
+                    new int[]{
+                        curr[0],
+                        nums2[nums2Idx + 1],
+                        nums2Idx + 1
+                    }
+                );
+            }
+        }
+
         return result;
     }
 }
