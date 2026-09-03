ca129d520adeba831496d060783b778000ed9053
harshrathod9021-maker <harshrathod9021@gmail.com>
2026-09-02 23:10:37 -0600

    Time: 8 ms (91.17%), Space: 49.3 MB (26.86%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,22 +1,32 @@
-class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        ArrayList<int[]> list = new ArrayList<>();
-        for (int[] x : intervals) {
-            list.add(x);
-        }
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i)[0] <= list.get(i - 1)[1]) {
-                list.get(i)[0] = list.get(i - 1)[0];
-                list.get(i)[1] = Math.max(
-                        list.get(i)[1],
-                        list.get(i - 1)[1]
-                );
-                list.remove(i - 1);
+import java.util.*;
 
-                i--;
+class Solution {
+
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        List<int[]> list = new ArrayList<>();
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            int s = intervals[i][0];
+            int e = intervals[i][1];
+
+            if (s <= end) {
+                end = Math.max(e, end);
+            } else {
+                list.add(new int[]{start, end});
+                start = s;
+                end = e;
             }
         }
+
+        list.add(new int[]{start, end});
+
         return list.toArray(new int[list.size()][]);
     }
 }
