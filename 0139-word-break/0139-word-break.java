bdfcd2e8c35d66cc7f18b9d2136ff3e36cc66fd7
harshrathod9021-maker <harshrathod9021@gmail.com>
2026-09-10 21:38:21 -0600

    Time: 3 ms (80.57%), Space: 43.4 MB (80.69%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,21 +1,72 @@
+// class Solution {
+//     public boolean wordBreak(String s, List<String> wordDict) {
+//         Map<String, Boolean> memo = new HashMap<>();
+//         Set<String> wordSet = new HashSet<>(wordDict);
+//         return dfs(s, wordSet, memo);
+//     }
+    
+//     private boolean dfs(String s, Set<String> wordSet, Map<String, Boolean> memo) {
+//         if (memo.containsKey(s)) return memo.get(s);
+//         if (wordSet.contains(s)) return true;
+//         for (int i = 1; i < s.length(); i++) {
+//             String prefix = s.substring(0, i);
+//             if (wordSet.contains(prefix) && dfs(s.substring(i), wordSet, memo)) {
+//                 memo.put(s, true);
+//                 return true;
+//             }
+//         }
+//         memo.put(s, false);
+//         return false;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Map<String, Boolean> memo = new HashMap<>();
-        Set<String> wordSet = new HashSet<>(wordDict);
-        return dfs(s, wordSet, memo);
-    }
-    
-    private boolean dfs(String s, Set<String> wordSet, Map<String, Boolean> memo) {
-        if (memo.containsKey(s)) return memo.get(s);
-        if (wordSet.contains(s)) return true;
-        for (int i = 1; i < s.length(); i++) {
-            String prefix = s.substring(0, i);
-            if (wordSet.contains(prefix) && dfs(s.substring(i), wordSet, memo)) {
-                memo.put(s, true);
-                return true;
+
+        int n = s.length();
+
+        // dp[i] = true if first i characters can be formed
+        boolean[] dp = new boolean[n + 1];
+
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (String word : wordDict) {
+
+                int len = word.length();
+
+                if (i >= len && dp[i - len]) {
+
+                    if (s.substring(i - len, i).equals(word)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
             }
         }
-        memo.put(s, false);
-        return false;
+
+        return dp[n];
     }
 }
