207605e70b042a6476f12beff8e1f109637e7903
harshrathod9021-maker <harshrathod9021@gmail.com>
2026-09-12 09:53:32 -0600

    Time: 2 ms (91.16%), Space: 43 MB (60.54%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -1,40 +1,24 @@
 class Solution {
     public int lastStoneWeightII(int[] stones) {
-        int sum = 0;
-        for (int stone : stones) {
-            sum += stone;
-        }
-        
-        boolean[][] memo = new boolean[stones.length + 1][sum / 2 + 1];
-        
-        for (int i = 0; i <= stones.length; i++) {
-            memo[i][0] = true;
-        }
-        
-        for (int i = sum / 2; i >= 0; i--) {
-            if (canMakeSum(stones, stones.length, i, memo)) {
-                return sum - 2 * i;
+        int n = stones.length, sum = 0;
+        for (int w : stones)
+            sum += w;
+        int half = sum / 2;
+
+        boolean[] dp = new boolean[half + 1];
+        dp[0] = true;
+
+        for (int w : stones) {
+            for (int j = half; j >= w; j--) {
+                dp[j] = dp[j] || dp[j - w];
             }
         }
-        
+
+        for (int j = half; j >= 0; j--) {
+            if (dp[j]) {
+                return sum - 2 * j;
+            }
+        }
         return 0;
-    }
-    
-    private boolean canMakeSum(int[] stones, int i, int j, boolean[][] memo) {
-        if (j == 0) return true;
-        if (i == 0) return false;
-        
-        if (memo[i][j]) {
-            return true;
-        }
-        
-        if (stones[i - 1] <= j) {
-            memo[i][j] = canMakeSum(stones, i - 1, j - stones[i - 1], memo) || 
-                         canMakeSum(stones, i - 1, j, memo);
-        } else {
-            memo[i][j] = canMakeSum(stones, i - 1, j, memo);
-        }
-        
-        return memo[i][j];
     }
 }
